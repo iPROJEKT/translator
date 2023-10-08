5b9e2923038d79e924113ddc58898091aaa25e2c
our ame <you@example.com>
2023-10-08 21:11:31 +0300

    crud const

--- db/crud.py
+++ db/crud.py
@@ -9,25 +9,25 @@ async def create_user(
         message_data: Message,
         translate_text: str
 ):
-    db_room = UserHistory(
+    result = UserHistory(
         user_id=message_data.from_user.id,
         primary_text=message_data.text,
         translate_text=translate_text
     )
     async with AsyncSessionLocal() as session:
-        session.add(db_room)
+        session.add(result)
         await session.commit()
-        await session.refresh(db_room)
-    return db_room
+        await session.refresh(result)
+    return result
 
 
 async def get_histiry(
         user_id: int
 ):
     async with AsyncSessionLocal() as session:
-        res = await session.execute(
+        result = await session.execute(
             select(UserHistory.translate_text).where(
                 UserHistory.user_id == user_id
             )
         )
-    return res
+    return result
